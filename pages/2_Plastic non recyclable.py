5e4d88a88ee90ee48ecb11d6852711247edd962a
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-02 17:04:48 +0530

    Update 2_Plastic non recyclable.py

--- pages/2_Plastic non recyclable.py
+++ pages/2_Plastic non recyclable.py
@@ -1,12 +1,10 @@
-'''
-Packaging
-=========
-
-This module contains `PyInstaller <http://www.pyinstaller.org/>`_ hooks in
-order to assist in the process of building binary packages. PyInstaller allows
-you to produce stand-alone, self-contained executables of your Kivy app for
-Windows, Linux and Mac.
-
-For more information, please see the `PyInstaller website
-<http://www.pyinstaller.org/>`_
-'''
+import streamlit as st
+st.title('Rudra Environment solutions Ltd')
+st.image('Rudra.png')
+st.markdown('''Pick up information
+From society/shop/school on 2nd & 4th Saturdays of every month pick up by Rudra.
+''')
+st.markdown('''Contact Information
+020 25448900 
+9373053235
+''')
