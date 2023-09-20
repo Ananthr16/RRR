e6620f42d99c1eabe356b910863c82c58c30e705
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-20 21:55:30 +0530

    Update 2_Plastic non recyclable.py

--- pages/2_Plastic non recyclable.py
+++ pages/2_Plastic non recyclable.py
@@ -1,10 +1,22 @@
 import streamlit as st
-st.title('Rudra Environment solutions Ltd')
+st.title('Non recyclable plastic waste')
 #st.image('Rudra.png')
-st.markdown('''Pick up information
-From society/shop/school on 2nd & 4th Saturdays of every month pick up by Rudra.
+st.markdown('''Non-recyclable plastic waste refers to types of plastic materials that are not easily or economically recyclable through conventional recycling processes.
+These materials are typically difficult to recycle due to various reasons, such as their composition, contamination, or the lack of recycling infrastructure and technology to handle them effectively.
 ''')
-st.markdown('''Contact Information
-020 25448900 
-9373053235
+st.markdown('''The most commonly recycled plastics are:
+•	1 – Polyethylene Terephthalate (PET) – water bottles and plastic trays
+•	2 – High Density Polyethylene (HDPE) – milk cartoons and shampoo bottles
+•	5 – Polypropylene (PP) – margarine tubs and ready-meal trays
+Somewhat recyclable plastics (at specialist facilities) include:
+•	3 – Polyvinyl Chloride (PVC) – piping
+•	4 – Low Density Polyethylene (LDPE) – food bags
+•	6 – Polystyrene (PS) – plastic cutlery
+Incredibly hard to recycle plastics include crisp packets, salad bags, plastic wrap and more.
+
 ''')
+st.markdown(''' Recycler for non recyclable plastic waste - Rudra Environment solutions Ltd
+''')
+st.markdown('''All plastic waste much be clean and dry. Proper care must be taken to ensure there is no wet waste mixed.''')
+st.markdown('''Contact information - 020 25448900, 9373053235''')
+
