3a80d954b101770c2b88e8a57e454787db839602
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-23 18:57:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ from streamlit_extras.switch_page_button import switch_page
 
 #setting the page
 st.set_page_config(initial_sidebar_state = 'collapsed')
-st.title('Reduce, Recycle, Reuse')
-st.subheader('''The RRR Web Application uses a 2 step ML Model backed up with multiple data sets as well as a customized data set to identify the object and the material after which it provides ways to reduce, reuse, and recycle the obejct.  ''') 
-
 col = st.columns(4)
 with col[0]:
 	if st.button("AI Model"):
@@ -21,3 +18,7 @@ with col[1]:
 with col[2]:
 	if st.button("lll"):
 		st.write('hello')
+st.title('Reduce, Recycle, Reuse')
+st.subheader('''The RRR Web Application uses a 2 step ML Model backed up with multiple data sets as well as a customized data set to identify the object and the material after which it provides ways to reduce, reuse, and recycle the obejct.  ''') 
+
+
