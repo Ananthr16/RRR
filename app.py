0bab34e13c766aa3c9141aad4bdb9f454c1ad847
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-23 18:52:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ st.set_page_config(initial_sidebar_state = 'collapsed')
 st.title('Reduce, Recycle, Reuse')
 st.subheader('''The RRR Web Application uses a 2 step ML Model backed up with multiple data sets as well as a customized data set to identify the object and the material after which it provides ways to reduce, reuse, and recycle the obejct.  ''') 
 
-if st.button("AI Model"):
-	switch_page('AI Model')
-if st.button("Plastic(recyclable)"):
-	switch_page('Plastic(recyclable')
+col = st.columns(4)
+with col[0]:
+	if st.button("AI Model"):
+		switch_page('AI Model')
+with col[1]:
+	if st.button("Plastic(recyclable)"):
+		switch_page('Plastic(recyclable)')
+with col[2]:
+	if st.button("")
