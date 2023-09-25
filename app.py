8fe36323900868ee736da6e8a9d5ceaf620fdbc3
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-25 23:05:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from streamlit_extras.switch_page_button import switch_page
 
 #setting the page
 st.set_page_config(initial_sidebar_state = 'collapsed')
-col = st.columns(4)
+col = st.columns(6)
 with col[0]:
 	if st.button("AI Model"):
 		switch_page('AI Model')
@@ -18,6 +18,20 @@ with col[1]:
 with col[2]:
 	if st.button("lll"):
 		st.write('hello')
+with col[3]:
+	if st.button("Plastic non recyclable"):
+		switch_page('Plastic non recyclable')
+with col[4]:
+	if st.button("Paper\Cardboard"):
+		switch_page('Paper\Cardboard')
+with col[5]:
+	if st.button("E-Waste"):
+		switch_page('E-Waste')
+
+
+
+
+
 st.title('Reduce, Recycle, Reuse')
 st.subheader('''The RRR Web Application uses a 2 step ML Model backed up with multiple data sets as well as a customized data set to identify the object and the material after which it provides ways to reduce, reuse, and recycle the obejct.  ''') 
 
