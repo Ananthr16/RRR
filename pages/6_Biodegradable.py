7aedfa6a50a34a29cf0ec30ca61ed61e7a64893b
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-30 23:05:31 +0530

    Update 6_Biodegradable.py

--- pages/6_Biodegradable.py
+++ pages/6_Biodegradable.py
@@ -17,6 +17,7 @@ st.markdown(''' Biodegradable waste (organic) is generated from kitchen waste an
 st.markdown('''
 The precautions to be taken with respect to kitchen waste before composting is given here : ''')
 
+
 st.markdown(''' The commercial composting organisation contacts are given below
 \n MobiTrash - https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwil0ZiO7dKBAxXLP3AKHRm1AA0QFnoECAoQAQ&url=https%3A%2F%2Fwww.mobitrash.in%2F&usg=AOvVaw2CUH_i72_ZxySPZ98nhNgR&opi=89978449
 ''')
