dcb525d7f29a3af701ec09861afb5230efb0a3c8
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-30 23:23:15 +0530

    Update 7_Non-Biodegradable.py

--- pages/7_Non-Biodegradable.py
+++ pages/7_Non-Biodegradable.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+#setting the page
+if st.button('Back'):
+	switch_page('app')
+
+st.title('Non-Biodegradable waste')
+st.markdown(''' Non-Biodegradable waste (dry waste) can be catagorised as follows :
+''')
+asd  = st.columns(6)
+
+
+
+
 
