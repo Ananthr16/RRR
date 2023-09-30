105b943c443a0909e9554fbac275535fc57acdc3
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-30 23:16:20 +0530

    Update 6_Biodegradable.py

--- pages/6_Biodegradable.py
+++ pages/6_Biodegradable.py
@@ -19,6 +19,8 @@ The precautions to be taken with respect to kitchen waste before composting is g
 if st.button('Conditions for composting'):
 		switch_page('ConditionsComposting')
 
+st.markdown(''' In garden waste palm and coconut tree fronds (leaf) or the leaflets should not be included ''')
+
 
 st.markdown(''' The commercial composting organisation contacts are given below
 \n MobiTrash - https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwil0ZiO7dKBAxXLP3AKHRm1AA0QFnoECAoQAQ&url=https%3A%2F%2Fwww.mobitrash.in%2F&usg=AOvVaw2CUH_i72_ZxySPZ98nhNgR&opi=89978449
