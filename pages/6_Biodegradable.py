88bfebf16504c9bbe81c28693c4c870729bd00d0
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-30 20:55:05 +0530

    Update 6_Biodegradable.py

--- pages/6_Biodegradable.py
+++ pages/6_Biodegradable.py
@@ -6,5 +6,14 @@ if st.button('Back'):
 	switch_page('app')
 
 st.title('Biodegradable Waste')
+st.markdown(''' Biodegradable waste (organic) is generated from kitchen waste and garden waste 
+\n Biodegradable waste should be composted.
+\n Composting can be done:
+\n a) At the individual household level. 
+\n b) At the residential complex level.
+\n c) At the community level.
+\n d) By commercial composting organisations. 
+''')
+
 
 
