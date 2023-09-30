580a4d04e7a40f2a1fb5f7c9c6fdf0644844204c
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-09-30 20:16:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ from streamlit_extras.switch_page_button import switch_page
 
 #setting the page
 st.set_page_config(initial_sidebar_state = 'collapsed',layout = 'wide')
+
+st.title('Reduce, Recycle, Reuse')
+st.subheader('''RRR: Reduce, Recycle, Reuse 
+RRR is a Web-based App which helps you decide what you can do with any item or material in order to dispose of it sustainability, and with least harm to the environment. 
+ \n It uses a 2 step ML Model backed up with multiple data sets as well as a customized data set. 
+ \n This helps to identify the object/material using your camera, after which it provides ways to reduce, reuse, and recycle it. 
+\n Use it to lead a healthy and less wasteful life. And protect our earth - the only home we have. ''') 
+
+
 col = st.columns(9)
 with col[0]:
 	if st.button("AI Model"):
@@ -25,16 +34,5 @@ with col[6]:
 with col[8]:
 	if st.button("E-Waste"):
 		switch_page('E-Waste')
-
-
-
-
-
-st.title('Reduce, Recycle, Reuse')
-st.subheader('''RRR: Reduce, Recycle, Reuse 
-RRR is a Web-based App which helps you decide what you can do with any item or material in order to dispose of it sustainability, and with least harm to the environment. 
- \n It uses a 2 step ML Model backed up with multiple data sets as well as a customized data set. 
- \n This helps to identify the object/material using your camera, after which it provides ways to reduce, reuse, and recycle it. 
-\n Use it to lead a healthy and less wasteful life. And protect our earth - the only home we have. ''') 
 
 
