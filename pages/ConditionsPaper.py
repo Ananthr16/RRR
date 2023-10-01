3c8a0c95a21302ac211d40786bd0d885f5924804
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-01 23:06:48 +0530

    Update ConditionsPaper.py

--- pages/ConditionsPaper.py
+++ pages/ConditionsPaper.py
@@ -3,6 +3,6 @@ from streamlit_extras.switch_page_button import switch_page
 
 #setting the page
 if st.button('Back'):
-	switch_page('Biodegradable')
+	switch_page('Paper')
 
 st.markdown(''' Segregate clean and dry paper and cardboard at source, bring to RRC ''')
