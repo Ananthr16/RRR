a496aadbfe070e6c66366f67732dc9b5b2e1f49a
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-01 23:27:08 +0530

    Update Plastic.py

--- pages/Plastic.py
+++ pages/Plastic.py
@@ -5,24 +5,26 @@ from streamlit_extras.switch_page_button import switch_page
 if st.button('Back'):
 	switch_page('Non-Biodegradable')
 
-st.title('Biodegradable Waste')
-st.markdown(''' Biodegradable waste (organic) is generated from kitchen waste and garden waste 
-\n Biodegradable waste should be composted.
-\n Composting can be done:
-\n a) At the individual household level. 
-\n b) At the residential complex level.
-\n c) At the community level.
-\n d) By commercial composting organisations. 
+st.title('Plastic Waste')
+st.markdown('''Plastic waste is a pressing environmental issue with far-reaching consequences.
+n\ It refers to discarded plastic products and materials that accumulate in landfills, oceans, and natural habitats.
+''')
+
+st.markdown(''' n\
+Plastic waste can be repurposed into creative household items. To get innovative ideas on how plastic waste can be repurposed use the AI Model.
+n\ Recycling of plastic waste can be done only in dedicated plastic recycling facilites 
 ''')
 st.markdown('''
-The precautions to be taken with respect to kitchen waste before composting is given here : ''')
+n\ The precautions to be taken with respect to plastic before handing it over to recyclers are given below : ''')
 if st.button('Conditions for composting'):
 		switch_page('ConditionsPlastic')
 
-st.markdown(''' In garden waste palm and coconut tree fronds (leaf) or the leaflets should not be included ''')
 
 
-st.markdown(''' The commercial composting organisation contacts are given below
-\n MobiTrash - https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwil0ZiO7dKBAxXLP3AKHRm1AA0QFnoECAoQAQ&url=https%3A%2F%2Fwww.mobitrash.in%2F&usg=AOvVaw2CUH_i72_ZxySPZ98nhNgR&opi=89978449
-''')
+
+st.markdown(''' The contacts for organisations which collect plastic waste which is to be recycled are given below
+\n a) For non-recyclable Plastic - Rudra Environment Solutions Ltd.
+contact - 020 25448900, 9373053235
+
+
 
