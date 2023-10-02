f9e4fe67a3861a4f1e848452d5112508885da932
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-02 14:06:02 +0530

    Update Plastic.py

--- pages/Plastic.py
+++ pages/Plastic.py
@@ -14,6 +14,9 @@ st.markdown('''
 \n Plastic waste can be repurposed into creative household items. To get innovative ideas on how plastic waste can be repurposed use the AI Model.
 \n Recycling of plastic waste can be done only in dedicated plastic recycling facilites 
 ''')
+if st.button('AI-Model'):
+	switch_page('AI Model')
+
 st.markdown('''
 \n The precautions to be taken with respect to plastic before handing it over to recyclers are given below : ''')
 if st.button('Conditions for recycling plastic waste'):
