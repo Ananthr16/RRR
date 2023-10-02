15b6e42fad6db2989ed7685afc4f74e8c7736a11
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-02 14:10:44 +0530

    Update Paper.py

--- pages/Paper.py
+++ pages/Paper.py
@@ -5,7 +5,7 @@ from streamlit_extras.switch_page_button import switch_page
 if st.button('Back'):
 	switch_page('Non-Biodegradable')
 
-st.set_page_config(initial_sidebar_state = 'collapsed',layout = 'wide')
+
 
 
 st.title('Paper Waste')
