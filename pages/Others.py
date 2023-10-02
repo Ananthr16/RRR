5ec01fc2b8d877ae573fc252b9d34544451c23fe
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-02 21:28:05 +0530

    Update Others.py

--- pages/Others.py
+++ pages/Others.py
@@ -1,1 +1,31 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+#setting the page
+if st.button('Back'):
+	switch_page('Non-Biodegradable')
+
+
+
+
+st.title('Paper Waste')
+st.markdown(''' Paper waste refers to discarded or unused paper products that are no longer needed or have reached the end of their useful life. 
+\n It encompasses any paper materials that are thrown away, recycled, or otherwise disposed of in a manner that does not involve their continued use.
+''')
+if st.button('Conditions to recycle paper waste'):
+	switch_page('ConditionsPaper')
+	
+st.markdown('''
+Paper can be recycled through
+\n a) Collection centres for recycling paper
+\n b) Community recycling centres 
+\n d) By commercial recyclers.  ''')
+
+
+
+
+st.markdown(''' The commercial recyclers contacts are given below
+\n Scrapcash 
+contact details for Scrapcash : Siddanth Ingale 9112323252
+''')
 
