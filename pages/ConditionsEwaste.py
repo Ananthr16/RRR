cf8bf9ab1ee6027e8c3f963b7037b4e1784fce50
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-02 21:17:15 +0530

    Update ConditionsEwaste.py

--- pages/ConditionsEwaste.py
+++ pages/ConditionsEwaste.py
@@ -3,4 +3,21 @@ from streamlit_extras.switch_page_button import switch_page
 
 #setting the page
 if st.button('Back'):
-	switch_page('Biodegradable')
+	switch_page('E-Waste')
+
+st.title('Conditions to hand over E-Waste to commercials recyclers')
+
+st.markdown(''' 
+Remove Batteries: If possible, remove batteries from devices.
+Batteries should be recycled separately because they can contain hazardous materials.
+
+\n Remove Accessories: Detach any peripherals, cables, or accessories from the electronic devices. These can often be recycled separately.
+
+\n Separate Components: If you have the knowledge and tools, consider disassembling the electronic devices into their main components, such as circuit boards, plastics, and metals.
+Separating components can facilitate more efficient recycling.
+
+\n Check for Hazardous Materials: Identify any hazardous materials present in your e-waste, such as mercury-containing bulbs, and ensure they are handled and recycled separately according to local regulations.
+
+\n Label Hazardous Items: Clearly label any hazardous materials or components so that the recycling organization is aware of their presence.
+
+''')
