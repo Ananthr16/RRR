40f9cd61f1a1233331de47086224f1d1f58ea36e
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-02 15:10:53 +0530

    Update ConditionsMetal.py

--- pages/ConditionsMetal.py
+++ pages/ConditionsMetal.py
@@ -4,3 +4,15 @@ from streamlit_extras.switch_page_button import switch_page
 #setting the page
 if st.button('Back'):
 	switch_page('Metal')
+
+st.title('Conditions to recycle metal waste')
+st.markdown('''
+\n Cleanliness: Metal waste should be cleaned to remove contaminants such as dirt, oil, or grease. 
+Clean metals are easier to process and yield higher-quality recycled materials.
+\n Separation: Metals should be separated by type to facilitate recycling. 
+Commonly recycled metals include aluminum, steel, copper, brass, and others. 
+Separating them at the source streamlines the recycling process.
+\n No Mixing with Non-Metals: Metal waste should not be mixed with non-metallic materials like plastics, glass, or organic waste. 
+Such contamination can impede the recycling process and affect the quality of the recycled metal.
+\n Size Reduction (if necessary): Large metal items may need to be reduced in size before recycling, either by cutting, crushing, or shredding. 
+This makes them more manageable for processing.
