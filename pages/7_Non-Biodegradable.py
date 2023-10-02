042621e738cf8a71be75e287576c3bb38ca8ba40
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-02 14:11:16 +0530

    Update 7_Non-Biodegradable.py

--- pages/7_Non-Biodegradable.py
+++ pages/7_Non-Biodegradable.py
@@ -4,7 +4,7 @@ from streamlit_extras.switch_page_button import switch_page
 #setting the page
 if st.button('Back'):
 	switch_page('app')
-st.set_page_config(initial_sidebar_state = 'collapsed',layout = 'wide')
+
 
 st.title('Non-Biodegradable waste')
 
