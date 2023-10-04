afbba8312b8abfc602b5cb2de142c295fc7f904d
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-04 22:22:50 +0530

    Update Others.py

--- pages/Others.py
+++ pages/Others.py
@@ -13,23 +13,30 @@ st.markdown(''' Other types of waste includes Mats, rubber, coir, cotton, synthe
 ''')
 
 st.markdown(''' The recycling organisations that provide services to recycle these miscellaneous wastes ''')
-st.markdown('''
-\n For footware - Barefoot Foundation. The footware is to be tied in pairs and dropped at Joggers Park. 
-Sanyogita Kudale: 984808860 
-\n For BEDSHEETS, CURTAINS
-AND SOFA COVERS - Call Mrs Lakhani & drop it at gate of Landmark Garden society, Kalyani Nagar.
+
+st.subtitle('For footware')
+st.markdown('''- BAREFOOT FOUNDATION. The footware is to be tied in pairs and dropped at Joggers Park. 
+ Contact for Barefoot Foundation - Sanyogita Kudale : 984808860 
+ \n ''')
+st.subtitle('For BEDSHEETS, CURTAINS, AND SOFA COVERS')
+st.markdown('''Call Mrs Lakhani & drop it at gate of Landmark Garden society, Kalyani Nagar.
 Rajeshwari Lakhani: 9765606409
-\n For THERMOCOL (EPS- Expanded Polystyrene)
-NOT
-STYROFOAM (XPS- Extruded Polystyrene Foam) - KK Nag Pvt Ltd. and G.D.Environmental Pvt Ltd. (GDEPL)
-For KK Nag Pvt Ltd. contact - Ghare (RecyCole): 9021619509 OR Dipankar Nandi : 9373336522
-For G.D.Environmental Pvt Ltd. (GDEPL) - Abhijeet Datar (GDEPL) : 9822043916
-\n 
-To donate CLOTHES, TOYS, BOOKS  - 
-\n Ishanya Foundation. Contact - Bindu Nair : 9960033433
+\n ''')
+st.subtitle('For THERMOCOL (EPS- Expanded Polystyrene) NOT STYROFOAM (XPS- Extruded Polystyrene Foam)')
+
+st.markdown('''KK Nag Pvt Ltd. Contact - Ghare (RecyCole): 9021619509 OR Dipankar Nandi : 9373336522 
+\n ''')
+
+st.markdown(''' G.D.Environmental Pvt Ltd. (GDEPL) - Abhijeet Datar (GDEPL) : 9822043916
+n\ ''')
+
+
+st.subtitle('To donate CLOTHES, TOYS, BOOKS')   
+st.markdown(''' \n Ishanya Foundation. Contact - Bindu Nair : 9960033433
 \n Poornam EcovisionFoundation. Contact - Supriya Kokane: 7558324287
 \n Swach VCollect. Conatct - Prasanna : 9765999494
 ''')
+
 
 
 
