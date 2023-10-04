82a4654509069a0ab12056720d611be855a1f010
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-05 00:53:27 +0530

    Update Plastic.py

--- pages/Plastic.py
+++ pages/Plastic.py
@@ -5,28 +5,30 @@ from streamlit_extras.switch_page_button import switch_page
 if st.button('Back'):
 	switch_page('Non-Biodegradable')
 
-
-st.title('Plastic Waste')
-st.markdown('''Plastic waste is a pressing environmental issue with far-reaching consequences.
+column = st.columns(2)
+with column[0]:
+	
+	st.title('Plastic Waste')
+	st.markdown('''Plastic waste is a pressing environmental issue with far-reaching consequences.
 \n It refers to discarded plastic products and materials that accumulate in landfills, oceans, and natural habitats.
 ''')
 
-st.markdown(''' 
+	st.markdown(''' 
 \n Plastic waste can be repurposed into creative household items. To get innovative ideas on how plastic waste can be repurposed use the AI Model.
 \n Recycling of plastic waste can be done only in dedicated plastic recycling facilites 
 ''')
-if st.button('AI-Model'):
+	if st.button('AI-Model'):
 	switch_page('AI Model')
 
-st.markdown('''
+	st.markdown('''
 \n The precautions to be taken with respect to plastic before handing it over to recyclers are given below : ''')
-if st.button('Conditions for recycling plastic waste'):
+	if st.button('Conditions for recycling plastic waste'):
 		switch_page('ConditionsPlastic')
 
 
 
 
-st.markdown(''' The contacts for organisations which collect plastic waste which is to be recycled are given below
+	st.markdown(''' The contacts for organisations which collect plastic waste which is to be recycled are given below
 \n a) For non-recyclable Plastic waste - Rudra Environment Solutions Ltd.
 Contact - 020 25448900, 9373053235
 \n b) For recyclable Plastic waste - Scrapcash   Contact - 9112323252
