8b1bfc97dec00a3ad399b5927d349ab41f4ff8bb
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-05 01:05:43 +0530

    Update Glass.py

--- pages/Glass.py
+++ pages/Glass.py
@@ -6,21 +6,25 @@ if st.button('Back'):
 	switch_page('Non-Biodegradable')
 
 
-
-st.title('Glass Waste')
-st.markdown(''' Glass waste is a crucial component of sustainable waste management.
+column = st.columns(2)
+with column[0]:
+	
+	st.title('Glass Waste')
+	st.markdown(''' Glass waste is a crucial component of sustainable waste management.
 Glass is a versatile material that can be endlessly recycled without a loss in quality, making it an environmentally friendly option. 
 ''')
-st.markdown('''
+	st.markdown('''
 The precautions to be taken with respect to Glass Waste before handing over to commercial recyclers are given below : ''')
-if st.button('Conditions for Glass Waste recycling'):
+	if st.button('Conditions for Glass Waste recycling'):
 		switch_page('ConditionsGlass')
 
 
 
 
-st.markdown(''' The commercial Glass recycling organisation's contacts are given below
+	st.markdown(''' The commercial Glass recycling organisation's contacts are given below
 \n ScrapCash - Siddhant Ingale : 9112323252, Zuber Sayyed : 8308133133
 
 ''')
+
+
 
