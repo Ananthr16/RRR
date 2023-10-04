32098b0b2c2a8fef829629809839b781502a49b7
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-04 22:32:49 +0530

    Update Others.py

--- pages/Others.py
+++ pages/Others.py
@@ -8,33 +8,27 @@ if st.button('Back'):
 
 
 
-
-
-st.title('Other types of waste')
-st.markdown(''' Other types of waste includes Mats, rubber, coir, cotton, synthetic, Mops, Dusters, Footwear, Ceramic, Cosmetics, Wood, and Coconut shells
-''')
-
-st.markdown(''' The recycling organisations that provide services to recycle these miscellaneous wastes ''')
-
-st.subheader('For footware')
-st.markdown('''- BAREFOOT FOUNDATION. The footware is to be tied in pairs and dropped at Joggers Park. 
+column = st.columns(2)
+with column[0]:
+	st.title('Other types of waste')
+	st.markdown(''' Other types of waste includes Mats, rubber, coir, cotton, synthetic, Mops, Dusters, Footwear, Ceramic, Cosmetics, Wood, and Coconut shells
+ ''')
+	st.markdown(''' The recycling organisations that provide services to recycle these miscellaneous wastes ''')
+	st.subheader('For footware')
+	st.markdown('''- BAREFOOT FOUNDATION. The footware is to be tied in pairs and dropped at Joggers Park. 
  Contact for Barefoot Foundation - Sanyogita Kudale : 984808860 
  \n ''')
-st.subheader('For BEDSHEETS, CURTAINS, AND SOFA COVERS')
-st.markdown('''Call Mrs Lakhani & drop it at gate of Landmark Garden society, Kalyani Nagar.
+	st.subheader('For BEDSHEETS, CURTAINS, AND SOFA COVERS')
+	st.markdown('''Call Mrs Lakhani & drop it at gate of Landmark Garden society, Kalyani Nagar.
 Rajeshwari Lakhani: 9765606409
 \n ''')
-st.subheader('For THERMOCOL (EPS- Expanded Polystyrene) NOT STYROFOAM (XPS- Extruded Polystyrene Foam)')
-
-st.markdown('''KK Nag Pvt Ltd. Contact - Ghare (RecyCole): 9021619509 OR Dipankar Nandi : 9373336522 
+	st.subheader('For THERMOCOL (EPS- Expanded Polystyrene) NOT STYROFOAM (XPS- Extruded Polystyrene Foam)')
+	st.markdown('''KK Nag Pvt Ltd. Contact - Ghare (RecyCole): 9021619509 OR Dipankar Nandi : 9373336522 
 \n ''')
-
-st.markdown(''' G.D.Environmental Pvt Ltd. (GDEPL) - Abhijeet Datar (GDEPL) : 9822043916
+	st.markdown(''' G.D.Environmental Pvt Ltd. (GDEPL) - Abhijeet Datar (GDEPL) : 9822043916
 n\ ''')
-
-
-st.subheader('To donate CLOTHES, TOYS, BOOKS')   
-st.markdown(''' \n Ishanya Foundation. Contact - Bindu Nair : 9960033433
+	st.subheader('To donate CLOTHES, TOYS, BOOKS')   
+	st.markdown(''' \n Ishanya Foundation. Contact - Bindu Nair : 9960033433
 \n Poornam EcovisionFoundation. Contact - Supriya Kokane: 7558324287
 \n Swach VCollect. Conatct - Prasanna : 9765999494
 ''')
