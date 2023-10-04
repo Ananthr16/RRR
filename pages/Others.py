69735ff2be7430e5c864edf057374cf5b8293899
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-04 22:27:34 +0530

    Update Others.py

--- pages/Others.py
+++ pages/Others.py
@@ -16,15 +16,15 @@ st.markdown(''' Other types of waste includes Mats, rubber, coir, cotton, synthe
 
 st.markdown(''' The recycling organisations that provide services to recycle these miscellaneous wastes ''')
 
-st.subtitle('For footware')
+st.subheader('For footware')
 st.markdown('''- BAREFOOT FOUNDATION. The footware is to be tied in pairs and dropped at Joggers Park. 
  Contact for Barefoot Foundation - Sanyogita Kudale : 984808860 
  \n ''')
-st.subtitle('For BEDSHEETS, CURTAINS, AND SOFA COVERS')
+st.subheader('For BEDSHEETS, CURTAINS, AND SOFA COVERS')
 st.markdown('''Call Mrs Lakhani & drop it at gate of Landmark Garden society, Kalyani Nagar.
 Rajeshwari Lakhani: 9765606409
 \n ''')
-st.subtitle('For THERMOCOL (EPS- Expanded Polystyrene) NOT STYROFOAM (XPS- Extruded Polystyrene Foam)')
+st.subheader('For THERMOCOL (EPS- Expanded Polystyrene) NOT STYROFOAM (XPS- Extruded Polystyrene Foam)')
 
 st.markdown('''KK Nag Pvt Ltd. Contact - Ghare (RecyCole): 9021619509 OR Dipankar Nandi : 9373336522 
 \n ''')
@@ -33,7 +33,7 @@ st.markdown(''' G.D.Environmental Pvt Ltd. (GDEPL) - Abhijeet Datar (GDEPL) : 98
 n\ ''')
 
 
-st.subtitle('To donate CLOTHES, TOYS, BOOKS')   
+st.subheader('To donate CLOTHES, TOYS, BOOKS')   
 st.markdown(''' \n Ishanya Foundation. Contact - Bindu Nair : 9960033433
 \n Poornam EcovisionFoundation. Contact - Supriya Kokane: 7558324287
 \n Swach VCollect. Conatct - Prasanna : 9765999494
