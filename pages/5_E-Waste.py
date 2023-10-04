4257eb4c7def84831b0c4d0244cc677bc23599de
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-05 01:36:54 +0530

    Update 5_E-Waste.py

--- pages/5_E-Waste.py
+++ pages/5_E-Waste.py
@@ -5,25 +5,30 @@ from streamlit_extras.switch_page_button import switch_page
 if st.button('Back'):
 	switch_page('Non-Biodegradable')
 
+column = st.columns(2)
+with column[0]:
+	
 
-
-st.title('E-Waste')
-st.markdown(''' E-waste, short for electronic waste, refers to discarded electronic and electrical equipment that has reached the end of its useful life. 
+	st.title('E-Waste')
+	st.markdown(''' E-waste, short for electronic waste, refers to discarded electronic and electrical equipment that has reached the end of its useful life. 
 This category of waste includes a wide range of devices and appliances, such as smartphones, laptops, televisions, refrigerators, and more. 
 ''')
-st.markdown('''
+	st.markdown('''
 The precautions to be taken with respect to E-Waste before handing it over to commercial E-Waste recycling organisations are given below : ''')
-if st.button('Conditions for E-Waste'):
+	if st.button('Conditions for E-Waste'):
 		switch_page('ConditionsEwaste')
 
 
 
 
-st.markdown(''' The commercial E-Waste recycling oragnisations and their contacts are given below
+	st.markdown(''' The commercial E-Waste recycling oragnisations and their contacts are given below
 \n Scrapcash - Siddhant Ingale : 9112323252 
 \n Poornam Ecovision - Supriya Kokane : 7558324287
 \n Hi Tech RecyclingIndia - Manoj Patil : 020 66521000
 ''')
+
+with column[1]:
+	st.image('E-waste.png')
 
 
 
