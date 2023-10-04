3695368533a82a433b7ae3cdc0d2da677d3b78e9
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-05 01:16:44 +0530

    Update Metal.py

--- pages/Metal.py
+++ pages/Metal.py
@@ -5,20 +5,25 @@ from streamlit_extras.switch_page_button import switch_page
 if st.button('Back'):
 	switch_page('Non-Biodegradable')
 
-
-st.title('Metal Waste')
-st.markdown(''' Metal waste generated from households consists of various items made of metal that are no longer needed or have reached the end of their usable life. 
+column = st.columns(2)
+with column[0]:
+	
+	st.title('Metal Waste')
+	st.markdown(''' Metal waste generated from households consists of various items made of metal that are no longer needed or have reached the end of their usable life. 
 Common examples include kitchen utensils, appliances, old furniture, bicycles, and metal containers.
 ''')
-st.markdown('''
+	st.markdown('''
 The precautions to be taken with respect to Metal waste before handing it over to recyclers is given below ''')
-if st.button('Conditions for Metal Waste'):
+	if st.button('Conditions for Metal Waste'):
 		switch_page('ConditionsMetal')
 
 
 
 
-st.markdown(''' The commercial metal recycling organisation contacts are given below
+	st.markdown(''' The commercial metal recycling organisation contacts are given below
 \n ScrapCash -  Zuber Sayyed : 8308133133, 
 \n ScrapCash - Siddhant Ingale : 9112323252
 ''')
+
+
+	
