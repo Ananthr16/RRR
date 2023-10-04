3a57cddab97877468c664b415049f57a2eea3b70
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-04 22:16:02 +0530

    Update Others.py

--- pages/Others.py
+++ pages/Others.py
@@ -11,21 +11,42 @@ if st.button('Back'):
 st.title('Other types of waste')
 st.markdown(''' Other types of waste includes Mats, rubber, coir, cotton, synthetic, Mops, Dusters, Footwear, Ceramic, Cosmetics, Wood, and Coconut shells
 ''')
-if st.button('Conditions to recycle paper waste'):
-	switch_page('ConditionsPaper')
-	
-	
+
+st.markdown(''' The recycling organisations that provide services to recycle these miscellaneous wastes ''')
 st.markdown('''
-Paper can be recycled through
-\n a) Collection centres for recycling paper
-\n b) Community recycling centres 
-\n d) By commercial recyclers.  ''')
+\n For footware - Barefoot Foundation. The footware is to be tied in pairs and dropped at Joggers Park. 
+Sanyogita Kudale: 984808860 
+\n For BEDSHEETS, CURTAINS
+AND SOFA COVERS - Call Mrs Lakhani & drop it at gate of Landmark Garden society, Kalyani Nagar.
+Rajeshwari Lakhani: 9765606409
+\n For THERMOCOL (EPS- Expanded Polystyrene)
+NOT
+STYROFOAM (XPS- Extruded Polystyrene Foam) - KK Nag Pvt Ltd. and G.D.Environmental Pvt Ltd. (GDEPL)
+For KK Nag Pvt Ltd. contact - Ghare (RecyCole): 9021619509 OR Dipankar Nandi : 9373336522
+For G.D.Environmental Pvt Ltd. (GDEPL) - Abhijeet Datar (GDEPL) : 9822043916
+\n 
+To donate CLOTHES, TOYS, BOOKS  - 
+\n Ishanya Foundation. Contact - Bindu Nair : 9960033433
+\n Poornam EcovisionFoundation. Contact - Supriya Kokane: 7558324287
+\n Swach VCollect. Conatct - Prasanna : 9765999494
+'''
 
 
 
 
-st.markdown(''' The commercial recyclers contacts are given below
-\n Scrapcash 
-contact details for Scrapcash : Siddanth Ingale 9112323252
-''')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
