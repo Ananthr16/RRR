ae19032b03237f735dd6c8ab59e5431ce7113f1d
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-05 01:51:28 +0530

    Update Hazardous.py

--- pages/Hazardous.py
+++ pages/Hazardous.py
@@ -5,28 +5,27 @@ from streamlit_extras.switch_page_button import switch_page
 if st.button('Back'):
 	switch_page('app')
 
+column = st.columns(2)
+with column[0]:
+	
+	st.title('Hazardous Waste')
+	st.markdown(''' Hazardous waste refers to any waste material that poses a substantial threat to human health, the environment, or both, due to its chemical, physical, or biological characteristics.
+\n These wastes are typically produced by industrial processes, laboratories, and various other sources, but some hazardous materials can also be found in common household products.
+\n Mixing hazardous waste with normal waste can have serious consequences, including environmental contamination and health risks.
+\n Here are some common household hazardous wastes and the potential consequences of improper disposal:
 
-st.title('Biodegradable Waste')
-st.markdown(''' Biodegradable waste (organic) is generated from kitchen waste and garden waste 
-\n Biodegradable waste should be composted.
-\n Composting can be done:
-\n a) At the individual household level. 
-\n b) At the residential complex level.
-\n c) At the community level.
-\n d) By commercial composting organisations. 
+
 ''')
-st.markdown('''
-The precautions to be taken with respect to kitchen waste before composting is given here : ''')
-if st.button('Conditions for composting'):
-		switch_page('ConditionsComposting')
-
-st.markdown(''' In garden waste palm and coconut tree fronds (leaf) or the leaflets should not be included ''')
+	st.subheader('The precautions to be taken with respect to kitchen waste before composting is given here ')
+	st.markdown(''' Chemical Product Labels: Read product labels carefully, follow usage instructions, and dispose of the empty containers as hazardous waste if necessary.
+\nLocal Household Hazardous Waste Collection: Many communities have designated collection programs or facilities for household hazardous waste. Take advantage of these services for safe disposal.
 
 
-st.markdown(''' The commercial composting organisation contacts are given below
-\n MobiTrash - https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwil0ZiO7dKBAxXLP3AKHRm1AA0QFnoECAoQAQ&url=https%3A%2F%2Fwww.mobitrash.in%2F&usg=AOvVaw2CUH_i72_ZxySPZ98nhNgR&opi=89978449   Nilesh Bhagat: 9881722393 
-\n GREEN THUMB - Firdosh Roowalla: 9766403305
-\n PROEARTH / INORA - Hamsa Iyer: 9892126134 / Babasaheb: 8446667634
+
+
+	st.markdown(''' The commercial Hazardous waste recycling organisation's contacts are given below
+\n BIOMEDICAL WASTE - PASSCO(Injection needles, dressing material to be kept separately at home). Contact - 020 25467096 
 ''')
+
 
 
