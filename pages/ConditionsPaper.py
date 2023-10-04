086677015806fc10082ed6f6d94889b06ec2d259
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-04 22:56:15 +0530

    Update ConditionsPaper.py

--- pages/ConditionsPaper.py
+++ pages/ConditionsPaper.py
@@ -8,3 +8,9 @@ if st.button('Back'):
 st.title('Conditions to hand over Paper waste to commercials recyclers')
 
 st.markdown(''' Segregate clean and dry paper and cardboard at source, bring to RRC ''')
+st.markdown(''' 
+Remove Staples and Paperclips: Remove any staples, paperclips, or other metal fasteners from the paper. Small metal pieces can damage recycling machinery.
+\n Flatten Cardboard: Flatten cardboard boxes to save space and make transportation easier. This also helps prevent unnecessary waste in recycling bins.
+\n Avoid Contaminated Paper:Avoid recycling paper that is heavily contaminated with materials like wax, plastic coatings, or foil. These can be challenging to recycle and may need to be disposed of separately.
+\n Clean the Paper: Try to remove any food residue, liquids, or other contaminants from the paper. Paper that is heavily soiled or contaminated may not be suitable for recycling.
+''')
