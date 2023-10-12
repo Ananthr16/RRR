896dd89862ab13844e4b1fdfd2e902bbb13f5f76
Ananthr16 <138586672+Ananthr16@users.noreply.github.com>
2023-10-12 18:56:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,18 +37,16 @@ with col2:
 with col1:
 	st.title('R R R')
 	st.subheader('''RRR: Reduce, Recycle, Reuse 
+ 	
 RRR is a Web-based App which helps you decide what you can do with any item or material in order to dispose of it sustainability, and with least harm to the environment. 
  \n It uses a 2 step ML Model backed up with multiple data sets as well as a customized data set. 
  \n This helps to identify the object/material using your camera, after which it provides ways to reduce, reuse, and recycle it. 
 \n Use it to lead a healthy and less wasteful life. And protect our earth - the only home we have. ''') 
-count = 0
-colum = st.columns(1)
-with colum[0]:
 	if st.button("AI Model"):
 		switch_page('AI Model')
 	
 
-
+count = 0
 st.subheader('''Waste can be categorised as follows :''')
 
 
